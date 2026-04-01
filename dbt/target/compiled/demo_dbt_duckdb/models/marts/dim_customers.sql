d87172557cc0e666f8b8267922dc031e47086eb5
Arnaud Stephan <astephan@littlebigcode.fr>
2026-04-01 12:06:41 +0200

    Modif gitignore

--- dbt/target/compiled/demo_dbt_duckdb/models/marts/dim_customers.sql
+++
@@ -1,7 +0,0 @@
-select
-    customer_id,
-    full_name,
-    email,
-    country,
-    signup_date
-from warehouse.analytics_staging.stg_customers
--- dbt/target/compiled/demo_dbt_duckdb/models/marts/fct_orders.sql
+++
@@ -1,7 +0,0 @@
-select
-    o.order_id,
-    o.customer_id,
-    o.order_date,
-    o.amount,
-    o.status
-from warehouse.analytics_staging.stg_orders o
