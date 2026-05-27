d323f16abac51129a93fc13ef39e45390c8378aa
Arnaud Stephan <astephan@littlebigcode.fr>
2026-05-27 15:13:48 +0200

    Suppression

--- dbt/models/marts/dim_customers.sql
+++
@@ -1,7 +0,0 @@
-select
-    customer_id,
-    full_name,
-    email,
-    country,
-    signup_date
-from {{ ref('stg_customers') }}
--- dbt/models/marts/fct_orders.sql
+++
@@ -1,7 +0,0 @@
-select
-    o.order_id,
-    o.customer_id,
-    o.order_date,
-    o.amount,
-    o.status
-from {{ ref('stg_orders') }} o
